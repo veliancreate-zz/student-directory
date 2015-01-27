f081550033548c20426639418a991fb19642c923
veliancreate <julian_veling@hotmail.com>
2015-01-27 11:19:05 +0000

    added new functions

--- directory.rb
+++ directory.rb
@@ -1,20 +1,31 @@
-# let's put all students into an array
-        students = [
-          "Dr. Hannibal Lecter",
-          "Darth Vader",
-          "Nurse Ratched",
-          "Michael Corleone",
-          "Alex De Large",
-          "The Alien",
-          "Terminator",
-          "Freddy Kruger",
-          "The Joker"
-        ]
-        # and then print them
-        puts "The students of my cohort at Makers Academy"
-        puts "-------------"
-        students.each do |student|
-          puts student
-        end
-        # finally, we print the total
-        puts "Overall, we have #{students.length} great students"
+#let's put all students into an array
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex De Large",
+  "The Alien",
+  "Terminator",
+  "Freddy Kruger",
+  "The Joker"
+]
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
+end
+
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.length} great students"
+end 
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
