df2a8779739b9f3f130f12fe90ba049bdf43f8fe
veliancreate <julian_veling@hotmail.com>
2015-01-27 12:42:17 +0000

    added place of birth to information

--- directory.rb
+++ directory.rb
@@ -1,17 +1,19 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the name and place of birth of the students"
+  puts "To finish, just hit return thrice"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
+  place = gets.chomp
   # while the name is not empty, repeat this code
-  while !name.empty? do    
+  while !name.empty? && !place.empty? do    
     # add the student hash to the array
-    students << {:name => name, :cohort => :november}    
+    students << {:name => name, :cohort => :november, :place=>place}    
     puts "Now we have #{students.length} students"
     # get another name from the user
     name = gets.chomp
+    place = gets.chomp
   end
   # return the array of students
   students
@@ -25,7 +27,7 @@ end
 def print(students)
   i=0
   while i<students.length
-  	puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
+  	puts "#{i+1}. #{students[i][:name]}, #{students[i][:place]} (#{students[i][:cohort]} cohort)"
   	i+=1
   end	
 end
