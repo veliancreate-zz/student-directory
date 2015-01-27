569bda417d6d66f2967a07876a36c890eaddde88
veliancreate <julian_veling@hotmail.com>
2015-01-27 11:44:50 +0000

    added each with index to puts student number

--- directory.rb
+++ directory.rb
@@ -23,8 +23,8 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]})"
+  students.each_with_index do |student,num|
+    puts "#{num+1}:#{student[:name]} (#{student[:cohort]})"
   end
 end
 
