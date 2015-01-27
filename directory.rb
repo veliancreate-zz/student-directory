c73bf06e691506470ff6acb0d3d1de9b6ced05fa
veliancreate <julian_veling@hotmail.com>
2015-01-27 14:05:14 +0000

    refactored code

--- directory.rb
+++ directory.rb
@@ -1,41 +1,71 @@
+@students = []
 def input_students
   puts "Please enter the name and place of birth of the students"
-  puts "To finish, just hit return thrice"
-  # create an empty array
-  students = []
+  puts "To finish, just hit return thrice"  
   # get the first name
   name = gets.chomp
   place = gets.chomp
+  cohort = gets.chomp
   # while the name is not empty, repeat this code
-  while !name.empty? && !place.empty? do    
+  while !name.empty? && !place.empty? && !cohort.empty? do    
     # add the student hash to the array
-    students << {:name => name, :cohort => :november, :place=>place}    
-    puts "Now we have #{students.length} students"
+    @students << {:name => name, :cohort => cohort, :place=>place}    
+    puts "Now we have #{@students.length} students"
     # get another name from the user
     name = gets.chomp
     place = gets.chomp
+    cohort =gets.chomp
   end
   # return the array of students
-  students
+  @students
 end
+
+def show_students
+	print_header
+	print_students
+	print_footer
+end	
 
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "-------------"
 end
 
-def print(students)
-  i=0
-  while i<students.length
-  	puts "#{i+1}. #{students[i][:name]}, #{students[i][:place]} (#{students[i][:cohort]} cohort)"
-  	i+=1
-  end	
+def print_students
+	@students.each do |student|
+    	puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end	
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.length} great students"
+def print_footer
+  puts "Overall, we have #{@students.length} great students"
 end 
+
+def print_menu
+	puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end	
+
+def process(selection) 
+	case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you mean, try again"
+    end
+end	
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
 #nothing happens until we call the methods
-students = input_students
-print(students)
-print_footer(students)
+interactive_menu
