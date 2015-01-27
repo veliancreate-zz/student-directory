a08818f088a38dd65f6717257301b3545e6bf265
veliancreate <julian_veling@hotmail.com>
2015-01-27 11:04:46 +0000

    puts student list

---
+++ directory.rb
@@ -0,0 +1,17 @@
+#first we print the list of students
+puts "The students of my cohort at Makers Academy"
+puts "-------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex De Large"
+puts "The Alien"
+puts "Terminator"
+puts "Freddy Kruger"
+puts "The Joker"
+#finally, we print the total
+print "Overall, we have "
+#it’s important that print() doesn’t add new line characters
+print 9
+puts " great students"
