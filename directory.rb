03cf9a71508b2b8dffe8af1c9294b04ab5e5e92a
veliancreate <julian_veling@hotmail.com>
2015-01-27 11:14:27 +0000

    added iteration through the student array to puts the students on screen

--- directory.rb
+++ directory.rb
@@ -1,14 +1,20 @@
-student_count=9
-#first we print the list of students
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex De Large"
-puts "The Alien"
-puts "Terminator"
-puts "Freddy Kruger"
-puts "The Joker"
-puts "We have #{student_count} great students"
+# let's put all students into an array
+        students = [
+          "Dr. Hannibal Lecter",
+          "Darth Vader",
+          "Nurse Ratched",
+          "Michael Corleone",
+          "Alex De Large",
+          "The Alien",
+          "Terminator",
+          "Freddy Kruger",
+          "The Joker"
+        ]
+        # and then print them
+        puts "The students of my cohort at Makers Academy"
+        puts "-------------"
+        students.each do |student|
+          puts student
+        end
+        # finally, we print the total
+        puts "Overall, we have #{students.length} great students"
